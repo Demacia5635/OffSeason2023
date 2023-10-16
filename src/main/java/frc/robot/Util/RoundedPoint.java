007b9a39a364517e4fe39e5f7c1a65c5883098b1
DanielDaMagniv <danielvfx19@gmail.com>
2023-10-16 10:53:45 +0300

    Changed variables names

--- src/main/java/frc/robot/Util/RoundedPoint.java
+++ src/main/java/frc/robot/Util/RoundedPoint.java
@@ -10,8 +10,7 @@ public class RoundedPoint{
     Translation2d cPoint;
     Translation2d vectorAtoB;
     Translation2d vectorBtoC;
-
-
+    Translation2d vectorCool = new Translation2d(bPoint.getX(), bPoint.getY());
     public RoundedPoint(double radius, Translation2d aPoint, Translation2d bPoint, Translation2d cPoint){
         this.radius = radius;
         this.aPoint = aPoint;
@@ -20,29 +19,26 @@ public class RoundedPoint{
         vectorAtoB = bPoint.minus(aPoint);
         vectorBtoC = cPoint.minus(bPoint);
     }
-
-    
     private double getCornerAngle(){
         double bigAngle = vectorAtoB.times(-1).getAngle().getDegrees();
         double smallAngle = vectorBtoC.getAngle().getDegrees();
         return smallAngle - bigAngle;
     }
-
     private double getDirection(){
         double direction = (getCornerAngle() / 2) + vectorAtoB.times(-1).getAngle().getDegrees();
         return direction;
     }
-    private Translation2d getAngleCrossNormalVector(){
+    private Translation2d getAngleVector(){
         Translation2d vector = new Translation2d(1, new Rotation2d(Math.toRadians(getDirection())));
         return vector;
     }
-    private double getAngleCrossNormalVectorLength(){
+    private double getLength(){
         double length = Math.pow((Math.sin(Math.abs(getCornerAngle() / 2))), -1) * radius;
         System.out.println(length);
         return length;
     }
     private Translation2d getVector(){
-        return getAngleCrossNormalVector().times(getAngleCrossNormalVectorLength());
+        return getAngleVector().times(getLength());
     }
     public Translation2d getCenterCircle(){
         return getVector().plus(bPoint);
