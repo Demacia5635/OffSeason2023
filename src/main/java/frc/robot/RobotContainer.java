22d8a0fa4e460a88fd142fefab70164a0478e910
tomer-3431 <tomer10.katz@gmail.com>
2023-10-18 12:50:10 +0300

    make the Parallelogram drivable

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,13 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.XboxController;
+// import frc.robot.Constants.OperatorConstants;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.ParallelogramDrive;
+import frc.robot.subsystems.Parallelogram;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,12 +19,13 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
+  public Parallelogram parallelogram;
+  public XboxController controller;
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  // private final CommandXboxController m_driverController =
+  //     new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -43,12 +44,9 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
   /**
@@ -58,6 +56,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return new ParallelogramDrive(parallelogram, controller);
   }
 }
--- src/main/java/frc/robot/commands/ParallelogramDrive.java
+++ src/main/java/frc/robot/commands/ParallelogramDrive.java
@@ -4,12 +4,21 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Parallelogram;
 
-public class ParrelogramMoveBackAndForward extends CommandBase {
-  
+public class ParallelogramDrive extends CommandBase {
+
+  public Parallelogram parallelogram;
+  public XboxController controller;
+  public double yAxis;
+
   /** Creates a new Parrelogram. */
-  public ParrelogramMoveBackAndForward() {
+  public ParallelogramDrive(Parallelogram parallelogram, XboxController controller) {
+    this.parallelogram = parallelogram;
+    this.controller = controller;
+    addRequirements(parallelogram);
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
@@ -19,7 +28,10 @@ public class ParrelogramMoveBackAndForward extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    yAxis = controller.getLeftY();
+    parallelogram.setVel(yAxis*0.5);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
