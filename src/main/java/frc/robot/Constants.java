fbe20de92177bb0dd04a874b8972bc0031f38116
1HyperZ <omerzur06@gmail.com>
2023-10-18 16:57:04 +0300

    Moved Constatnts and created a dummy chassis

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -13,7 +17,67 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  }
+
+  public static final class VisionConstants {
+    public static final NetworkTable LIMELIGHT_TABLE1 = NetworkTableInstance.getDefault().getTable("limelight-ii");
+    public static final NetworkTable LIMELIGHT3_TABLE = NetworkTableInstance.getDefault().getTable("limelight-iii");
+
+    public static final NetworkTableEntry HAS_TARGET_ENTRY = LIMELIGHT_TABLE1.getEntry("tv"); // double not boolean
+    
+    /**
+     * An array of doubles with the following values:
+     * <p>
+     * [0] - meters from the corner of the blue alliance x axis
+     * <p>
+     * [1] - meters from the corner of the blue alliance y axis
+     * <p>
+     * [2] - meters from the the field carpet in the z axis
+     * <p>
+     * [3] - roll in degrees
+     * <p>
+     * [4] - pitch in degrees
+     * <p>
+     * [5] - yaw in degrees
+     */
+    public static final NetworkTableEntry ROBOT_POSE_ENTRY = LIMELIGHT_TABLE1.getEntry("botpose_wpiblue");
+
+    /**
+     * An array of doubles with the following values:
+     * <p>
+     * [0] - meters from the limelight to the april tag in the right direction
+     * <p>
+     * [1] - meters from the limelight to the april tag in the down direction
+     * <p>
+     * [2] - meters from the limelight to the april tag in the forward direction
+     * <p>
+     * [3] - pitch from the camera to the april tag in degrees
+     * <p>
+     * [4] - yaw from the camera to the april tag in degrees
+     * <p>
+     * [5] - roll from the camera to the april tag in degrees
+     */
+    public static final NetworkTableEntry CAMERA_TRANSLATION_ENTRY = LIMELIGHT_TABLE1
+            .getEntry("targetpose_cameraspace");
+
+    public static final double MAX_DISTANCE_FOR_LIMELIGHT = 3;
+
+    public static final double maxValidVelocity = 2.0; // m/s - vision data will be ingored when the robot is at a higher speed.
+    public static final double maxValidAngleDiff = 10;  // degrees - vision data will be ingored if the diffrence between vision and motors is higher than this.
+
+    public static final double VISION_ANGLE_TOLERANCE = 5;
+
+    public static final double LIMELIGHT2_YAW = 22.5;
+
+    public static final double LIMELIGHT3_YAW = 32.2;
+
+    public static final double MAX_SIDES_RATIO = 1.2;
+
+    public static final double VISION_TX_LIMIT = 5;
+
+    public static final double VISION_TA_LIMIT = 0.5;
   }
 }
--- src/main/java/frc/robot/utils/VisionUtils/Vision.java
+++ src/main/java/frc/robot/utils/VisionUtils/Vision.java
@@ -3,18 +3,107 @@ package frc.robot.utils.VisionUtils;
 import java.util.Arrays;
 import java.util.Comparator;
 
-import edu.wpi.first.math.MathSharedStore;
+import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.util.WPIUtilJNI;
 import edu.wpi.first.wpilibj.Timer;
 // import frc.robot.subsystems.Chassis;
 import frc.robot.subsystems.Chassis;
+import frc.robot.Constants;
+import frc.robot.Constants.VisionConstants;
 
 public class Vision {
     
-    private static final double maxValidVelocity = 2.0; // m/s - vision data will be ingored when the robot is at a higher speed.
-    private static final double maxValidAngleDiff = 10;  // degrees - vision data will be ingored if the diffrence between vision and motors is higher than this.
     
+    
+    SwerveDrivePoseEstimator poseEstimator;
+    Chassis chassis;
+    VisionData[] buf = new VisionData[3];
+    int lastData = -1;
+    double lastUpdateTime;
+
+    public Vision(Chassis chassis, SwerveDrivePoseEstimator estimator) {
+        this.chassis = chassis;
+        for(int i = 0; i < buf.length; i++) {
+            buf[i] = new VisionData(null, 0);
+        }
+    }
+    
+    public Pair<Pose2d, Double> getPose(){
+        if(chassis.getVelocity() > Constants.VisionConstants.maxValidVelocity ) {
+            return null;
+        }
+        double time = getTime();
+        lastData = next();
+        VisionData newVisionData = getVisionData();
+        if(newVisionData != null && newVisionData.pose != null){
+            buf[lastData] = getVisionData();
+            if(validBuf(time)) {
+                VisionData vData = median(buf);
+                if(vData != null && vData.pose != null) {
+                    Pair<Pose2d, Double> poseAndLatency = new Pair<Pose2d, Double>(vData.pose, getTime()-vData.timeStamp);
+                    lastUpdateTime = time;
+                    time = vData.timeStamp;
+                    for(VisionData vd : buf) {
+                        vd.recalc(time);
+                    }
+                    return poseAndLatency;
+                }
+            }
+            else{
+                VisionData dataBeforelastData = buf[lastData-1];
+                VisionData dataLastData = buf[lastData];
+                buf[lastData - 2] = dataBeforelastData;
+                buf[lastData - 1] = dataLastData;
+            }
+        }
+        return null;
+    }
+
+    private VisionData getVisionData() {
+        return null;
+    }
+
+
+
+    int next() {
+        return (lastData+1)%buf.length;
+    }
+
+    
+
+    public double getTime() {
+        return Timer.getFPGATimestamp();
+    }
+    
+    Comparator<VisionData> comperator = new Comparator<VisionData>() {
+        @Override
+        public int compare(VisionData data0, VisionData data1) {
+            return Double.compare(data0.diffrence, data1.diffrence);
+        }
+    };
+
+    private boolean validBuf(double time) {
+        double minTime = time - 1.2;
+        for(VisionData vData : buf) {
+            if(vData.timeStamp < minTime) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private VisionData median(VisionData[] visionDataArr){
+        Arrays.sort(visionDataArr,comperator);
+        return visionDataArr[visionDataArr.length/2];
+    }
+
+    public double lastUpdateLatency() {
+        return getTime() - lastUpdateTime;
+    }
+
+    public boolean validVisionPosition() {
+        return lastUpdateLatency() < 1;
+    }
+
     class VisionData{
     
         private Pose2d pose;
@@ -30,10 +119,19 @@ public class Vision {
                 clear();
             }
         } 
-        
+
+        void recalc(double time) {
+            // for newer data - recalc the twsit
+            if(timeStamp > time) {
+                setDiffrence();
+            } else {
+                clear();
+            }
+        }
+
         void setDiffrence() {
             Pose2d poseSample = poseEstimator.getSample(timeStamp);
-            if(poseSample != null && Math.abs(poseSample.getRotation().minus(pose.getRotation()).getDegrees()) < maxValidAngleDiff) {
+            if(poseSample != null && Math.abs(poseSample.getRotation().minus(pose.getRotation()).getDegrees()) < Constants.VisionConstants.maxValidAngleDiff) {
                 diffrence = poseSample.getTranslation().getDistance(pose.getTranslation());
             } else {
                 clear();
@@ -47,40 +145,5 @@ public class Vision {
         }
     }
 
-    SwerveDrivePoseEstimator poseEstimator;
-    Chassis chassis;
-    VisionData[] buf = new VisionData[3];
-    int lastData = -1;
-    double lastUpdateTime;
-
-    public Pose2d getPose(){
-
-    }
-
-    int next() {
-        return (lastData+1)%buf.length;
-    }
-
-    public Vision(Chassis chassis, SwerveDrivePoseEstimator estimator) {
-        this.chassis = chassis;
-        poseEstimator = estimator;
-        for(int i = 0; i < buf.length; i++) {
-            buf[i] = new VisionData(null, 0);
-        }
-    }
-
-    public double getTime() {
-        return Timer.getFPGATimestamp();
-    }
     
-    Comparator<VisionData> comperator = new Comparator<VisionData>() {
-        @Override
-        public int compare(VisionData data0, VisionData data1) {
-            return Double.compare(data0.diffrence, data1.diffrence);
-        }
-    };
-    private VisionData median(VisionData[] visionDataArr){
-        Arrays.sort(visionDataArr,comperator);
-        return visionDataArr[visionDataArr.length/2];
-    }
 }
