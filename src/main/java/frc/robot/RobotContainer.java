dc3f9591aae681fb7cf279c99f64bb3ef779745c
Shay Farjoon <shayfarjoon0@gmail.com>
2023-10-22 21:39:10 +0300

    Added roundedPath

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,7 +14,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-
+import frc.robot.commands.roundedPath;
 
 
 public class RobotContainer extends CommandBase{
@@ -22,10 +22,14 @@ public class RobotContainer extends CommandBase{
   Chassis chassis = new Chassis();
   
   CommandXboxController driverController = new CommandXboxController(DriverControllerPort);
-  DriveToPointWithCircle driveToPointWithCircle = new DriveToPointWithCircle(new Translation2d(1, 1), new Translation2d(0, 2), new Rotation2d(0), chassis);
+  //DriveToPointWithCircle driveToPointWithCircle = new DriveToPointWithCircle(new Translation2d(1, 1), new Translation2d(0, 2), new Rotation2d(0), chassis);
 
   Test test = new Test();
 
+  Translation2d[] points = {new Translation2d(0,0), new Translation2d(1,1), new Translation2d(3,-1), new Translation2d(4,0)};
+  double[] radius = {0.1,0.1};
+
+  roundedPath path = new roundedPath(chassis, points, radius, 0, 0);
 
 
 
@@ -51,6 +55,6 @@ public class RobotContainer extends CommandBase{
 
 
   public Command getAutonomousCommand() {
-    return test;
+    return path;
   }
 }
---
+++ src/main/java/frc/robot/commands/roundedPath.java
@@ -0,0 +1,66 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.io.Console;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Util.RoundedPoint;
+import frc.robot.Util.Trapezoid;
+import frc.robot.subsystems.Chassis;
+
+public class roundedPath extends CommandBase {
+  /** Creates a new roundedPath. */
+
+  double pathLength;
+  RoundedPoint[] corners;
+  Trapezoid trapezoid;
+  Chassis chassis;
+
+  public roundedPath(Chassis chassis,Translation2d[] points, double[] radius, double maxVel, double maxAcc) {
+    this.corners = new RoundedPoint[points.length - 2];
+    for(int i = 0; i < points.length - 2; i++)
+    {
+      corners[i] = new RoundedPoint(radius[i], points[i], points[i+1], points[i+2]);
+    }
+
+    this.chassis = chassis;
+    this.trapezoid = new Trapezoid(maxAcc, maxVel);
+
+    this.pathLength = 0;
+    for(int i = 0; i < corners.length - 1; i++)
+    {
+      this.pathLength += corners[i].getAtoCurvelength() + corners[i].getCurveLength();
+      System.out.println("added a to curve length : " + corners[i].getAtoCurvelength() + " | added curve length : " + corners[i].getCurveLength());
+    }
+    this.pathLength += corners[corners.length - 1].getAtoCurvelength() + corners[corners.length - 1].getCurveLength() + corners[corners.length - 1].getCtoCurvelength();
+    System.out.println("added a to curve length : " + corners[corners.length - 1].getAtoCurvelength() + " | added curve length : " + corners[corners.length - 1].getCurveLength() + " | added c to curve length : " + corners[corners.length - 1].getCtoCurvelength());
+    System.out.println("Path Length : " + this.pathLength);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    System.out.println("PathLength : " + pathLength);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
