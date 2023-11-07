9c9bc1ee17b3ff2b61c2e635a1d6c05fb75a4ede
Hatulim <tomer10.katz@gmail.com>
2023-11-07 09:27:28 +0200

    make ParallelogramStartToEnd caculator

--- src/main/java/frc/robot/util/ParallelogramCaculator.java
+++ src/main/java/frc/robot/util/ParallelogramCaculator.java
@@ -10,11 +10,28 @@ import frc.robot.subsystems.Parallelogram;
 public class ParallelogramCaculator {
     public Parallelogram parallelogram;
     public double angle;
+    private boolean isForward = true;
 
     public ParallelogramCaculator(Parallelogram parallelogram){
         this.parallelogram = parallelogram;
         angle = parallelogram.getAngle();
     }
 
-    
+    public int ParallelogramStartToEnd(double startAngle, double endAngle,double switchAngle, double pow){
+        if (!(endAngle <= angle) && isForward){
+            if ((startAngle < angle && angle < switchAngle)|| angle <= startAngle){
+                return 0;
+            } else if ((endAngle > angle && angle > switchAngle)|| angle <= switchAngle){
+                return 1;
+            }
+        } else {
+            isForward = false;
+            if ((endAngle > angle && angle > switchAngle)|| angle >= endAngle){
+                return 2;
+            } else if ((startAngle < angle && angle < switchAngle)|| angle <= switchAngle){
+                return 3;
+            }
+        }
+        return 4;
+    }
 }
