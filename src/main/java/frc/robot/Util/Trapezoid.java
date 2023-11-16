484fe655a71a962a02a50b2edb44b07443c539b1
nanonoam <noamper7@gmail.com>
2023-11-16 19:42:48 +0200

    Changed alg

--- src/main/java/frc/robot/Util/Trapezoid.java
+++ src/main/java/frc/robot/Util/Trapezoid.java
@@ -1,33 +1,48 @@
 package frc.robot.Util;
 
+import java.rmi.AccessException;
+
+import frc.robot.Constants;
+
 public class Trapezoid {
     private final double maxVelocity;
     private final double acceleration;
     private final double safeVelocity;
     private final double endVelocity;
+    double deltaV;
+    boolean firstTime;
 
     public Trapezoid(double maxVelocity, double acceleration, double safeVelocity, double endVelocity) {
         this.maxVelocity = maxVelocity;
         this.acceleration = acceleration;
         this.safeVelocity = safeVelocity;
         this.endVelocity = endVelocity;
+        deltaV = acceleration * Constants.CYCLE_DT;
+        firstTime = true;
     }
 
     public double calculate(double remainingDistance, double currentVelocity) {
-        double accelTime = maxVelocity / acceleration;
-        double accelDistance = 0.5 * acceleration * Math.pow(accelTime, 2);
-        if (currentVelocity < maxVelocity && remainingDistance > accelDistance) {
-            // accelerate
-            return currentVelocity + acceleration * 0.02;
+        if (requiredDistance(currentVelocity + deltaV, 0, -acceleration) <= remainingDistance ){
+            System.out.println("ACCEL: + " + Math.min(maxVelocity, currentVelocity + deltaV));
+            return Math.min(maxVelocity, currentVelocity + deltaV);
         }
-        else if (remainingDistance > accelDistance) {
-            // keep velocity
-            return maxVelocity;
-        }
-        else if (remainingDistance <= accelDistance) {
-            // deccelerate
-            return Math.max(Math.max(currentVelocity - acceleration * 0.02, safeVelocity), endVelocity);
-        }
-        return endVelocity;
+       else if(requiredDistance(currentVelocity, 0, -acceleration) <= remainingDistance){
+            System.out.println("KEEP: " + currentVelocity);
+            return currentVelocity;
+            
+       }
+       else{
+            System.out.println("DEACCEL: " + Math.max(currentVelocity-deltaV, endVelocity));
+            return Math.max(currentVelocity-deltaV, endVelocity);
+       }
+    
     }
+
+    public static double requiredDistance(double fromVelocity, double toVelocity, double acceleration) {
+        double time = (toVelocity - fromVelocity)/acceleration;
+        System.out.println("TIME: " + time);
+        System.out.println("ACCEL DISTANCE: " + time*fromVelocity + (acceleration*time*time)/2);
+        return time*fromVelocity + (acceleration*time*time)/2;
+    }
+
 }
