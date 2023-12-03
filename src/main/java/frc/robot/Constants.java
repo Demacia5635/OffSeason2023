9cd67acd033019bc397916bb987bab6f950d53fb
nanonoam <noamper7@gmail.com>
2023-12-03 20:43:12 +0200

    for buf 5

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -87,8 +87,8 @@ public final class Constants {
     public static final String photonCamera2Name = "";
     public static final int photonCameraNum1 = 1;
     public static final int photonCameraNum2 = 2;
-    public static final Pose2d robotCenterToCamera = new Pose2d(new Translation2d(0.23, 0.8),
-        Rotation2d.fromDegrees(144));
+    public static final Pose2d robotCenterToCamera = new Pose2d(new Translation2d(0.23, -0.8),
+        Rotation2d.fromDegrees(-35));
 
     public static final Transform3d robotCenterToCameraTransform = new Transform3d(new Pose3d(), new Pose3d(robotCenterToCamera));
 
--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.vision;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.NoSuchElementException;
 
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
@@ -23,15 +24,23 @@ import static frc.robot.Constants.VisionConstants.*;
 public class Vision extends SubsystemBase {
     Field2d poseEstimatorField;
     Field2d visionField;
+    Field2d visionField3;
+    Field2d visionField5;
+    double[] visionFieldArr;
+    double[] visionField3Arr;
+    double[] visionField5Arr;
     PhotonCamera camera1;
     PhotonCamera camera2;
     SwerveDrivePoseEstimator poseEstimator;
     PhotonPoseEstimator photonPoseEstimator;
     Chassis chassis;
-    VisionData[] buf = new VisionData[3];
+    VisionData[] buf3 = new VisionData[3];
+    VisionData[] buf5 = new VisionData[5];
     int lastData = -1;
+    int lastData5 = -1;
     double lastUpdateTime;
-    boolean firstRun;
+    double lastUpdateTime5;
+
 
     public Vision(Chassis chassis, SwerveDrivePoseEstimator estimator) {
         camera1 = photonCamera1;
@@ -43,59 +52,96 @@ public class Vision extends SubsystemBase {
             photonPoseEstimator = new PhotonPoseEstimator(AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile), PoseStrategy.AVERAGE_BEST_TARGETS, camera1, robotCenterToCameraTransform);
         } catch (IOException e) {
             e.printStackTrace();
-        }        
-        visionField = new Field2d();
+        }     
+        visionField= new Field2d();   
+        visionField3 = new Field2d();
+        visionField5 = new Field2d();
         poseEstimatorField = new Field2d();
-        firstRun = true;
-        for (int i = 0; i < buf.length; i++) {
-            buf[i] = new VisionData(null, 0);
+        visionFieldArr = new double[2];
+        visionField3Arr = new double[2];
+        visionField5Arr = new double[2];
+        for (int i = 0; i < buf3.length; i++) {
+            buf3[i] = new VisionData(null, 0);
         }
-        SmartDashboard.putData("field check pose estimator", poseEstimatorField);
-        SmartDashboard.putData("field check vision", visionField);
-        ;
+        for (int i = 0; i < buf5.length; i++) {
+            buf5[i] = new VisionData(null, 0);
+        }
+        // SmartDashboard.putData("field check pose estimator", poseEstimatorField);
+        SmartDashboard.putData("vision3", visionField3);
+        SmartDashboard.putData("vision5", visionField5);
+        SmartDashboard.putData("vision", visionField);
+        
+
+        
     }
     
     public void updateRobotPose() {
         double time = getTime();
         {
             if (validBuf(time)) {
-                VisionData vData = median(buf);
+                VisionData vData = median(buf3);
                 if (vData != null && vData.pose != null) {
                     // System.out.println(vData.pose);
                     poseEstimator.addVisionMeasurement(vData.pose, vData.timeStamp);
                     poseEstimatorField.setRobotPose(poseEstimator.getEstimatedPosition());
-                    visionField.setRobotPose(vData.pose);
+                    visionField3.setRobotPose(vData.pose);
                     lastUpdateTime = time;
                     time = vData.timeStamp;
-                    for (VisionData vd : buf) {
+                    for (VisionData vd : buf3) {
                         vd.recalc(time);
                     }
                 }
             } 
-            // else {
-            //     VisionData dataBeforelastData = buf[lastData - 1];
-            //     VisionData dataLastData = buf[lastData];
-            //     buf[lastData - 2] = dataBeforelastData;
-            //     buf[lastData - 1] = dataLastData;
-            // }
         }
+    }
+
+    public void updateRobotPose5() {
+        double time = getTime();
+        if (validBuf5(time)) {
+            VisionData vData5 = median(buf5);
+            if (vData5 != null && vData5.pose != null) {
+                // System.out.println(vData.pose);
+                // poseEstimator.addVisionMeasurement(vData5.pose, vData5.timeStamp);
+                // poseEstimatorField.setRobotPose(poseEstimator.getEstimatedPosition());
+                visionField5.setRobotPose(vData5.pose);
+                lastUpdateTime5 = time;
+                time = vData5.timeStamp;
+                for (VisionData vd : buf5) {
+                    vd.recalc(time);
+                }
+            }
+        } 
     }
 
     private void getNewVisionDataFromCamera() {
         if (chassis.getVelocity().getNorm() <= maxValidVelcity) {
             var PhotonUpdate = photonPoseEstimator.update();
-            if(PhotonUpdate != null && PhotonUpdate.get() != null){
-                var estimatedRobotPose = PhotonUpdate.get();
-                var estimatedPose = estimatedRobotPose.estimatedPose;
-                if(estimatedRobotPose != null){
-                    lastData = next();
-                    VisionData newVisionData = new VisionData(estimatedPose.toPose2d(), estimatedRobotPose.timestampSeconds);
-                    if (newVisionData != null && newVisionData.pose != null) {
-                        // if ((newVisionData.pose).getTranslation()
-                        //         .getDistance(estimatedRobotPose.getTranslation()) > maxDistanceOfCameraFromAprilTag)
-                        //     return; removed this filter because they use multiple april tags at the same time
-                        buf[lastData] = newVisionData;
-                    }   
+            if(PhotonUpdate != null){
+                try {
+                    var estimatedRobotPose = PhotonUpdate.get();
+                    var estimatedPose = estimatedRobotPose.estimatedPose;
+                    if(estimatedRobotPose != null){
+                        lastData = next();
+                        lastData5 = next5();
+                        VisionData newVisionData = new VisionData(estimatedPose.toPose2d(), estimatedRobotPose.timestampSeconds);
+                        if (newVisionData != null && newVisionData.pose != null) {
+                            // if ((newVisionData.pose).getTranslation()
+                            //         .getDistance(estimatedRobotPose.getTranslation()) > maxDistanceOfCameraFromAprilTag)
+                            //     return; removed this filter because they use multiple april tags at the same time
+                            buf3[lastData] = newVisionData;
+                            buf5[lastData5] = newVisionData;
+                            System.out.println("-----------------------------");
+                            System.out.println(lastData5 + " : " + buf5[lastData5].timeStamp);
+                            for(int i = 0; i < buf5.length; i++) {
+                                System.out.println(i + " : " + buf5[i].timeStamp);
+                                
+                            }
+                            System.out.println(validBuf5(getTime()));
+                            visionField.setRobotPose(newVisionData.pose);
+                        }   
+                    }
+                } catch (NoSuchElementException e) {
+                    // TODO: handle exception
                 }
             }
         }
@@ -106,17 +152,26 @@ public class Vision extends SubsystemBase {
     public void periodic() {
         super.periodic();
         getNewVisionDataFromCamera();
-        //getNewVisionDataFromCameraX(photonCameraNum2);
         updateRobotPose();
-        
-        
-        
+        updateRobotPose5();
+        Pose2d visionPose = visionField.getRobotPose();
+        SmartDashboard.putNumber("vision X", visionPose.getX());
+        SmartDashboard.putNumber("vision Y", visionPose.getY());
+        Pose2d vision3Pose = visionField3.getRobotPose();
+        SmartDashboard.putNumber("vision3 X", vision3Pose.getX());
+        SmartDashboard.putNumber("vision3 Y", vision3Pose.getY());
+        Pose2d vision5Pose = visionField5.getRobotPose();
+        SmartDashboard.putNumber("vision5 X", vision5Pose.getX());
+        SmartDashboard.putNumber("vision5 Y", vision5Pose.getY());    
+            
     }
 
     int next() {
-        return (lastData + 1) % buf.length;
+        return (lastData + 1) % buf3.length;
     }
-
+    int next5() {
+        return (lastData5 + 1) % buf5.length;
+    }
     public double getTime() {
         return Timer.getFPGATimestamp();
     }
@@ -130,7 +185,17 @@ public class Vision extends SubsystemBase {
 
     private boolean validBuf(double time) {
         double minTime = time - 1.2;
-        for (VisionData vData : buf) {
+        for (VisionData vData : buf3) {
+            if (vData.timeStamp < minTime) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private boolean validBuf5(double time) {
+        double minTime = time - 2;
+        for (VisionData vData : buf5) {
+            System.out.println(vData.timeStamp);
             if (vData.timeStamp < minTime) {
                 return false;
             }
@@ -142,7 +207,10 @@ public class Vision extends SubsystemBase {
         Arrays.sort(visionDataArr, comperator);
         return visionDataArr[visionDataArr.length / 2];
     }
-
+    private VisionData median5(VisionData[] visionDataArr) {
+        Arrays.sort(visionDataArr, comperator);
+        return visionDataArr[3];
+    }
     public double lastUpdateLatency() {
         return getTime() - lastUpdateTime;
     }
