d0f9a11623b19027e2100fc060695eb77f656f1d
1HyperZ <omerzur06@gmail.com>
2023-12-11 14:28:20 +0200

    addedAvg and removed // setDifference,TriedFixBuf5

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -12,6 +12,7 @@ import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -26,9 +27,9 @@ public class Vision extends SubsystemBase {
     Field2d visionField;
     Field2d visionField3;
     Field2d visionField5;
-    double[] visionFieldArr;
-    double[] visionField3Arr;
-    double[] visionField5Arr;
+    Field2d visionFieldavg3;
+    Field2d visionFieldavg5;
+
     PhotonCamera camera1;
     PhotonCamera camera2;
     SwerveDrivePoseEstimator poseEstimator;
@@ -36,6 +37,7 @@ public class Vision extends SubsystemBase {
     Chassis chassis;
     VisionData[] buf3 = new VisionData[3];
     VisionData[] buf5 = new VisionData[5];
+
     int lastData = -1;
     int lastData5 = -1;
     double lastUpdateTime;
@@ -56,10 +58,11 @@ public class Vision extends SubsystemBase {
         visionField= new Field2d();   
         visionField3 = new Field2d();
         visionField5 = new Field2d();
+        visionFieldavg3 = new Field2d();
+        visionFieldavg5 = new Field2d();
+
+
         poseEstimatorField = new Field2d();
-        visionFieldArr = new double[2];
-        visionField3Arr = new double[2];
-        visionField5Arr = new double[2];
         for (int i = 0; i < buf3.length; i++) {
             buf3[i] = new VisionData(null, 0);
         }
@@ -70,7 +73,10 @@ public class Vision extends SubsystemBase {
         SmartDashboard.putData("vision3", visionField3);
         SmartDashboard.putData("vision5", visionField5);
         SmartDashboard.putData("vision", visionField);
+
         
+        SmartDashboard.putData("visionavg3", visionFieldavg3);
+        SmartDashboard.putData("visionavg5", visionFieldavg5);
 
         
     }
@@ -80,11 +86,12 @@ public class Vision extends SubsystemBase {
         {
             if (validBuf(time)) {
                 VisionData vData = median(buf3);
+                VisionData vDataAvg = avg(buf3);
                 if (vData != null && vData.pose != null) {
-                    // System.out.println(vData.pose);
-                    poseEstimator.addVisionMeasurement(vData.pose, vData.timeStamp);
-                    poseEstimatorField.setRobotPose(poseEstimator.getEstimatedPosition());
+                    // poseEstimator.addVisionMeasurement(vData.pose, vData.timeStamp);
+                    // poseEstimatorField.setRobotPose(poseEstimator.getEstimatedPosition());
                     visionField3.setRobotPose(vData.pose);
+                    visionFieldavg3.setRobotPose(vDataAvg.pose);
                     lastUpdateTime = time;
                     time = vData.timeStamp;
                     for (VisionData vd : buf3) {
@@ -98,12 +105,13 @@ public class Vision extends SubsystemBase {
     public void updateRobotPose5() {
         double time = getTime();
         if (validBuf5(time)) {
-            VisionData vData5 = median(buf5);
+            VisionData vData5 = median5(buf5);
+            VisionData vDataAvg = avg(buf3);
             if (vData5 != null && vData5.pose != null) {
-                // System.out.println(vData.pose);
                 // poseEstimator.addVisionMeasurement(vData5.pose, vData5.timeStamp);
                 // poseEstimatorField.setRobotPose(poseEstimator.getEstimatedPosition());
                 visionField5.setRobotPose(vData5.pose);
+                visionFieldavg5.setRobotPose(vDataAvg.pose);
                 lastUpdateTime5 = time;
                 time = vData5.timeStamp;
                 for (VisionData vd : buf5) {
@@ -130,18 +138,19 @@ public class Vision extends SubsystemBase {
                             //     return; removed this filter because they use multiple april tags at the same time
                             buf3[lastData] = newVisionData;
                             buf5[lastData5] = newVisionData;
-                            System.out.println("-----------------------------");
-                            System.out.println(lastData5 + " : " + buf5[lastData5].timeStamp);
-                            for(int i = 0; i < buf5.length; i++) {
-                                System.out.println(i + " : " + buf5[i].timeStamp);
+                            // System.out.println("-----------------------------");
+                            // System.out.println(lastData5 + " : " + buf5[lastData5].timeStamp);
+                            // for(int i = 0; i < buf5.length; i++) {
+                            //     System.out.println(i + " : " + buf5[i].timeStamp);
                                 
-                            }
-                            System.out.println(validBuf5(getTime()));
+                            // }
+
+
                             visionField.setRobotPose(newVisionData.pose);
                         }   
                     }
                 } catch (NoSuchElementException e) {
-                    // TODO: handle exception
+                    System.out.println("No Robot Pose Found print");
                 }
             }
         }
@@ -155,27 +164,25 @@ public class Vision extends SubsystemBase {
         updateRobotPose();
         updateRobotPose5();
         Pose2d visionPose = visionField.getRobotPose();
-        SmartDashboard.putNumber("vision X", visionPose.getX());
-        SmartDashboard.putNumber("vision Y", visionPose.getY());
+        SmartDashboard.putNumber("no filter X", visionPose.getX());
+        SmartDashboard.putNumber("no filter Y", visionPose.getY());
         Pose2d vision3Pose = visionField3.getRobotPose();
-        SmartDashboard.putNumber("vision3 X", vision3Pose.getX());
-        SmartDashboard.putNumber("vision3 Y", vision3Pose.getY());
+        SmartDashboard.putNumber("buf 3 med X", vision3Pose.getX());
+        SmartDashboard.putNumber("buf 3 med Y", vision3Pose.getY());
         Pose2d vision5Pose = visionField5.getRobotPose();
-        SmartDashboard.putNumber("vision5 X", vision5Pose.getX());
-        SmartDashboard.putNumber("vision5 Y", vision5Pose.getY());    
+        SmartDashboard.putNumber("buf 5 med X", vision5Pose.getX());
+        SmartDashboard.putNumber("buf 5 med Y", vision5Pose.getY());    
+
+        Pose2d vision3avgPose = visionFieldavg3.getRobotPose();
+        SmartDashboard.putNumber("buf 3 avg X", vision3avgPose.getX());
+        SmartDashboard.putNumber("buf 3 avg Y", vision3avgPose.getY());
+        Pose2d vision5avgPose = visionFieldavg5.getRobotPose();
+        SmartDashboard.putNumber("buf 5 avg X", vision5avgPose.getX());
+        SmartDashboard.putNumber("buf 5 avg Y", vision5avgPose.getY());    
+
             
     }
-
-    int next() {
-        return (lastData + 1) % buf3.length;
-    }
-    int next5() {
-        return (lastData5 + 1) % buf5.length;
-    }
-    public double getTime() {
-        return Timer.getFPGATimestamp();
-    }
-
+    //util
     Comparator<VisionData> comperator = new Comparator<VisionData>() {
         @Override
         public int compare(VisionData data0, VisionData data1) {
@@ -183,19 +190,41 @@ public class Vision extends SubsystemBase {
         }
     };
 
+    public double getTime() {
+        return Timer.getFPGATimestamp();
+    }
+
+    public VisionData avg(VisionData[] visionDataArr) {
+
+        double averageX = 0.0;
+        double averageY = 0.0;
+        double averageRotation = 0.0;
+        double avarageTimeStamp = 0.0;
+
+        for (VisionData vData : visionDataArr) {
+            Pose2d pose = vData.pose;
+            averageX += pose.getTranslation().getX();
+            averageY += pose.getTranslation().getY();
+            averageRotation += pose.getRotation().getRadians();
+            avarageTimeStamp += vData.timeStamp;
+        }
+
+        averageX /= visionDataArr.length;
+        averageY /= visionDataArr.length;
+        averageRotation /= visionDataArr.length;
+        avarageTimeStamp /= visionDataArr.length;
+        // Create a new Pose2d with the calculated averages
+        return new VisionData((new Pose2d(averageX, averageY, new Rotation2d(averageRotation))), avarageTimeStamp);
+    }
+
+    // BUF 3
+    int next() {
+        return (lastData + 1) % buf3.length;
+    }
+
     private boolean validBuf(double time) {
         double minTime = time - 1.2;
         for (VisionData vData : buf3) {
-            if (vData.timeStamp < minTime) {
-                return false;
-            }
-        }
-        return true;
-    }
-    private boolean validBuf5(double time) {
-        double minTime = time - 2;
-        for (VisionData vData : buf5) {
-            System.out.println(vData.timeStamp);
             if (vData.timeStamp < minTime) {
                 return false;
             }
@@ -207,16 +236,41 @@ public class Vision extends SubsystemBase {
         Arrays.sort(visionDataArr, comperator);
         return visionDataArr[visionDataArr.length / 2];
     }
-    private VisionData median5(VisionData[] visionDataArr) {
-        Arrays.sort(visionDataArr, comperator);
-        return visionDataArr[3];
-    }
+
     public double lastUpdateLatency() {
         return getTime() - lastUpdateTime;
     }
 
     public boolean validVisionPosition() {
         return lastUpdateLatency() < 1;
+    }
+    // BUF 5
+    int next5() {
+        return (lastData5 + 1) % buf5.length;
+    }
+
+    private boolean validBuf5(double time) {
+        double minTime = time - 2;
+        for (VisionData vData : buf5) {
+            System.out.println(vData.timeStamp);
+            if (vData.timeStamp < minTime) {
+                return false;
+            }
+        }
+        return true;
+    }
+   
+    private VisionData median5(VisionData[] visionDataArr) {
+        Arrays.sort(visionDataArr, comperator);
+        return visionDataArr[3];
+    }
+   
+    public double lastUpdateLatency5() {
+        return getTime() - lastUpdateTime5;
+    }
+    
+    public boolean validVisionPosition5() {
+        return lastUpdateLatency5() < 1;
     }
 
     class VisionData {
@@ -229,7 +283,7 @@ public class Vision extends SubsystemBase {
             this.pose = pose;
             this.timeStamp = timeStamp;
             if (timeStamp > 0) {
-                // setDiffrence();
+                setDiffrence();
             } else {
                 clear();
             }
@@ -238,7 +292,7 @@ public class Vision extends SubsystemBase {
         void recalc(double time) {
             // for newer data - recalc the twsit
             if (timeStamp > time) {
-                // setDiffrence();
+                setDiffrence();
             } else {
                 clear();
             }
